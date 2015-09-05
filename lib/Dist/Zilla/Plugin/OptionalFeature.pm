129b63093ce5fa6db842c939270ce8deb4d0778c
Karen Etheridge <ether@cpan.org>
2015-09-05 11:05:31 -0700

    add some keywords to metadata

--- lib/Dist/Zilla/Plugin/OptionalFeature.pm
+++ lib/Dist/Zilla/Plugin/OptionalFeature.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::OptionalFeature;
 # ABSTRACT: Specify prerequisites for optional features in your distribution
+# KEYWORDS: plugin metadata prerequisites optional recommended prompt install
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.022';
